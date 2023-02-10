1ad95a5ab06f69674abaa0e9e8db4d014e3ff417
Ramka <whereisourgod@gmail.com>
2023-02-10 18:38:39 +1000

    t

---
+++ client/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Sockets;
+
+string ip = "127.0.0.1";
+int port = 55555;
+using TcpClient user = new TcpClient();
+Console.Write("Your name: ");
+string? userName = Console.ReadLine();
+StreamReader? reader = null;
+StreamWriter? writer = null;
+System.Console.WriteLine();
+try
+{
+    user.Connect(ip, port);
+    reader = new StreamReader(user.GetStream());
+    writer = new StreamWriter(user.GetStream());
+    if (reader is null || writer is null) return;
+    Task.Run(() => GetMessage(reader));
+    await SendMessage(writer);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    writer?.Close();
+    reader?.Close();
+}
+
+async Task GetMessage(StreamReader reader)
+{
+    while (true)
+    {
+        try
+        {
+            string? message = await reader.ReadLineAsync();
+            if (!string.IsNullOrEmpty(message)) Console.WriteLine(message);
+        }
+        catch
+        {
+            break;
+        }
+    }
+}
+
+async Task SendMessage(StreamWriter writer)
+{
+    await writer.WriteLineAsync(userName);
+    await writer.FlushAsync();
+    Console.WriteLine("<<<Press Enter to send message>>>");
+
+    while (true)
+    {
+        string? message = Console.ReadLine();
+        await writer.WriteLineAsync(message);
+        await writer.FlushAsync();
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,132 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+Server server = new Server();
+await server.Listening();
+class Server
+{
+    TcpListener server = new TcpListener(IPAddress.Any, 55555);
+    List<Users> users = new List<Users>();
+
+    protected internal void DropConnection(string id)
+    {
+        Users? user = users.FirstOrDefault(c => c.Id == id);
+        if (user != null)
+        {
+            users.Remove(user);
+            user.CloseAll();
+        }
+
+    }
+    protected internal async Task Listening()
+    {
+        try
+        {
+            server.Start();
+            Console.WriteLine("Server started...");
+
+            while (true)
+            {
+                TcpClient tcpClient = await server.AcceptTcpClientAsync();
+                Console.WriteLine($"Incoming connection: {tcpClient.Client.RemoteEndPoint}");
+                Users user = new Users(tcpClient, this);
+                users.Add(user);
+                Task.Run(user.StartChat);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            DisconnectUsers();
+        }
+    }
+
+    protected internal async Task WorkingMessager(string message, string id)
+    {
+        foreach (var user in users)
+        {
+            if (user.Id != id)
+            {
+                await user.writer.WriteLineAsync(message);
+                await user.writer.FlushAsync();
+            }
+        }
+    }
+    protected internal void DisconnectUsers()
+    {
+        foreach (var user in users)
+        {
+            user.CloseAll();
+        }
+        server.Stop();
+    }
+
+}
+class Users
+{
+    protected internal string Id { get; } = Guid.NewGuid().ToString();
+    protected internal StreamWriter writer { get; }
+    protected internal StreamReader reader { get; }
+    TcpClient user;
+    Server server;
+
+    public Users(TcpClient user, Server server)
+    {
+        this.user = user;
+        this.server = server;
+        var stream = user.GetStream();
+        reader = new StreamReader(stream);
+        writer = new StreamWriter(stream);
+    }
+
+    public async Task StartChat()
+    {
+        try
+        {
+            string? userName = await reader.ReadLineAsync();
+            string? message = "";
+            while (true)
+            {
+                try
+                {
+
+                    if (message != null)
+                    {
+                        message = await reader.ReadLineAsync();
+                        message = $"{userName}: {message}";
+                        Console.WriteLine(message);
+                        await server.WorkingMessager(message, Id);
+                    }
+
+                }
+                catch
+                {
+                    message = $"{userName} покинул чат";
+                    Console.WriteLine(message);
+                    await server.WorkingMessager(message, Id);
+                    break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+
+        }
+        finally
+        {
+            server.DropConnection(Id);
+        }
+    }
+
+    protected internal void CloseAll()
+    {
+        writer.Close();
+        reader.Close();
+        user.Close();
+    }
+
+}
